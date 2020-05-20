3e7077d038eeeee089f7fc430df547641bc1d113
G <gauthier.castro@ecs.co.uk>
2020-05-20 16:34:55 +0100

    added update_user.py: parse csv & update

---
+++ update_user.py
@@ -0,0 +1,74 @@
+import boto3
+import json
+
+import csv
+
+#################################################################
+########################### CONSTANTS ###########################
+#################################################################
+
+## connect instance ID
+CONNECT_INSTANCE_ID = "3ab95c31-5dad-482b-9b7e-f6929d3b2619"
+
+## users to update csv file
+UPDATE_FILEPATH = "user_hierarchy_updates.csv"
+
+## load connect API
+client = boto3.client('connect')
+
+#################################################################
+#################################################################
+
+
+def write_agent_update_file(path="user_hierarchy_updates.csv"):
+    with open(path, 'w') as f:
+        f.writelines("username,hierarchygroup\n")
+        for i in range(1, 21):
+            f.writelines(f"agent-{i:03},Bristol\n")
+def get_user_updates(csv_path):
+    '''read [csv_path] file and associate each column to a dict entry
+    return csv_csv: dict
+    '''
+    csv_dict = {
+        'username': [],
+        'hierarchygroup': [],
+    }
+
+    with open(csv_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row['username'], row['hierarchygroup'])
+            csv_dict['username'].append(row["username"])
+            csv_dict['hierarchygroup'].append(row["hierarchygroup"])
+    return csv_dict
+
+
+## write the file
+write_agent_update_file()
+
+## get updates to apply
+update_data = get_user_updates(UPDATE_FILEPATH)
+
+## get each user's id
+response = client.list_users(InstanceId=CONNECT_INSTANCE_ID)
+update_data["id"] = [user["Id"] for user in response["UserSummaryList"] if user["Username"] in update_data['username']]
+
+## get each hierarchy group's id
+response = client.list_user_hierarchy_groups(InstanceId=CONNECT_INSTANCE_ID,)
+hgs = {group["Name"]: group["Id"] for group in response['UserHierarchyGroupSummaryList']}
+
+
+response = []
+for name, id, hg in zip(update_data['username'], update_data['id'], update_data['hierarchygroup']):
+    print(f"{name:<10}\t{id}\t {hg}\t {hgs[hg]}")
+    # List user hierarchy groups
+    r = client.update_user_hierarchy(
+        HierarchyGroupId=hgs[hg],
+        UserId=id,
+        InstanceId=CONNECT_INSTANCE_ID
+    )
+    response.append(r)
+
+with open('connect_jsons/update_user_hierarchy.json', 'w') as f:
+    json.dump(response, f, indent=2)
+
