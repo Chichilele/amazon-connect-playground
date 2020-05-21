b7470deba7da0ad3343af7209115e4a7cfed9db0
G <gauthier.castro@ecs.co.uk>
2020-05-21 16:45:16 +0100

    lambda v2 with exceptions to cloudwatch

--- lambda.py
+++ lambda.py
@@ -13,42 +13,37 @@ s3_client = boto3.client('s3')
 CONNECT_INSTANCE_ID = "3ab95c31-5dad-482b-9b7e-f6929d3b2619"
 
 
+
 def get_user_updates(csv_path):
     '''read [csv_path] file and associate each column to a dict entry
     return csv_csv: dict
     '''
-    csv_dict = {
-        'username': [],
-        'hierarchygroup': [],
-    }
+    csv_dict = {}
 
     with open(csv_path, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            print(row['username'], row['hierarchygroup'])
-            csv_dict['username'].append(row["username"])
-            csv_dict['hierarchygroup'].append(row["hierarchygroup"])
+            csv_dict[row["username"]] = {"hg": row['hierarchygroup']}
     return csv_dict
 
-
 def update_each_user(update_data, hierarchy_groups):
-    response = {"sucess": {}, "fail": {}}
-    for name, id, hg in zip(update_data['username'], update_data['id'], update_data['hierarchygroup']):
-        print(f"{name:<10}\t{id}\t {hg}\t {hierarchy_groups[hg]}")
-        ## List user hierarchy groups
+    response = []
+    for name, user in update_data.items():
+        print({"username": f"{name:<10}", "user_id": f"{user.get('id', 'UNKOWN_USERID')}",
+                "hierarchy_group": f"{user['hg']}", "hierarchy_group_id": f"{hierarchy_groups.get(user['hg'], 'UNKOWN_HG_ID')}"})
+        
         try:
             r = connect_client.update_user_hierarchy(
-                HierarchyGroupId=hierarchy_groups[hg],
-                UserId=id,
+                HierarchyGroupId=hierarchy_groups.get(user['hg'], 'UNKOWN_HIERARCHY_GROUP_ID'),
+                UserId=user.get('id', 'UNKOWN_USER_ID'),
                 InstanceId=CONNECT_INSTANCE_ID
             )
-            response["success"][name] = r
-        except Exception as e: 
-            print(f"ERRPR \tcouldn't update {name}. The following error occured")
-            print(e)
-            response["fail"][name] = r
+        except Exception as e:
+            print(f"Exception... {type(e).__name__}: {e}")
+    response.append(r)
     
     return response
+
 
 def lambda_handler(event, context):
     for record in event['Records']:
@@ -67,17 +62,15 @@ def lambda_handler(event, context):
         
         ## get each user's id
         response = connect_client.list_users(InstanceId=CONNECT_INSTANCE_ID)
-        update_data["id"] = [user["Id"] for user in response["UserSummaryList"] if user["Username"] in update_data['username']]
+        for user in response["UserSummaryList"]:
+            if user["Username"] in update_data.keys():
+                update_data[user["Username"]]["id"] = user["Id"]
         
         ## get each hierarchy group's id
         response = connect_client.list_user_hierarchy_groups(InstanceId=CONNECT_INSTANCE_ID,)
         hierarchy_groups = {group["Name"]: group["Id"] for group in response['UserHierarchyGroupSummaryList']}
         
         ## update connect 
-        reponse = update_each_user(update_data, hierarchy_groups)
+        response = update_each_user(update_data, hierarchy_groups)
         
         return response
-
-
-        
-        
